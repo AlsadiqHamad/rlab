951b0ca7bfa36cb02d92d6628e22f851a0e5930d
Your Name <your@email.com>
2012-09-18 23:44:03 -0700

    first commit
    Signed-off-by: Your Name <el-sadig@mafgood.org>

---
+++ application/views/public/index.php
@@ -0,0 +1,21 @@
+<?php $this->load->view('global/header'); ?>
+
+<p>
+<label  for="item">username</label>
+<input  type="text"  id="item" name="item"/>
+<input type="submit"  id="submit" name="submit" value="send"/>
+</p>
+
+
+	
+<?php $this->load->view('global/footer'); ?>
+
+
+
+
+
+
+
+
+
+
